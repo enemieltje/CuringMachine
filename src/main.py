dfb676eeb7b3aa73a6816a18b3b1d18c91f3d783
Lord_Redst0ne <74739340+enemieltje@users.noreply.github.com>
2024-04-18 10:45:16 +0200

    create main class

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# The client class will start the program and contain all its components
+class Client:
+
+    # init is called when an instance of this class is created
+    def __init__(self):
+        print("start")
+
+
+# Create an instance of the Client class, and save it in a variable called client
+client = Client()
