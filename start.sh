31b5c960344a02ff76b06afc9addea5beaa8c1df
Lord_Redst0ne <74739340+enemieltje@users.noreply.github.com>
2024-04-25 12:30:02 +0200

    update gpio to rpi 5

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 python -m venv ./.venv
 
 ./.venv/bin/pip install numpy --upgrade
-./.venv/bin/pip install rpi.gpio
+./.venv/bin/pip install lgpio pigpio gpio
 ./.venv/bin/pip install gpiozero    # Button input
 ./.venv/bin/pip install hx711       # Load cell
 ./.venv/bin/pip install picamera2   # Camera
