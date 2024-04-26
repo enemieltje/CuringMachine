fa6aaa86382bde01b682ebaf5c74c6d65c4467b6
enemieltje <enemieltje@gmail.com>
2024-04-26 10:29:01 +0200

    added libcamera dependency

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ python -m venv ./.venv
 ./.venv/bin/pip install lgpio pigpio gpio
 ./.venv/bin/pip install gpiozero    # Button input
 ./.venv/bin/pip install hx711       # Load cell
-./.venv/bin/pip install picamera2   # Camera
+./.venv/bin/pip install libcamera picamera2   # Camera
 
 ./.venv/bin/python ./src/main.py
