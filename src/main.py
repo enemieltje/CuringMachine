b2e8d2e32f2cdc4075d3713fcc1c77f83e18306d
enemieltje <enemieltje@gmail.com>
2024-04-26 11:34:13 +0200

    added buttons to start/stop camera

--- src/main.py
+++ src/main.py
@@ -2,25 +2,12 @@
 from belt import Belt
 from server import Server
 from camera import Camera
-# The client class will start the program and contain all its components
-
-
-class Client:
-    belt: Belt
-    server: Server
-    camera: Camera
-
-    # init is called when an instance of this class is created
-    def __init__(self):
-        print("start")
-        self.camera = Camera()
-        self.camera.startStream()
-        self.server = Server()
-        self.server.startStreamServer()
-        self.camera.stopStream()
-        self.belt = Belt()
 
 
 # Create an instance of the Client class, and save it in a variable called client
 if __name__ == "__main__":
-    client = Client()
+    print("start")
+    camera = Camera()
+    Server.addCamera(camera)
+    Server.startStreamServer()
+    belt = Belt()
--- src/server.py
+++ src/server.py
@@ -3,65 +3,55 @@ import time
 import io
 import logging
 import socketserver
+import multiprocessing
 from http import server
-from camera import output
+from camera import output, Camera
 
 
 PAGE = """\
 <html>
 <head>
-<title>RaspberryTips Pi Cam Stream</title>
+<title>Curing Machine</title>
 </head>
 <body>
-<h1>Raspberry Tips Pi Camera Live Stream Demo</h1>
+<h1>Pi Camera Live Stream Demo</h1>
 <img src="stream.mjpg" width="640" height="480" />
+<a href="button/startcam">Start Preview</a>
+<a href="button/stopcam">Stop Preview</a>
 </body>
 </html>
 """
 
 
-class Server():
-    hostName = "0.0.0.0"
-    port: int
-    webServer: HTTPServer
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer, multiprocessing.Process):
+    allow_reuse_address = True
+    daemon_threads = True
 
-    def __init__(self, port=8080):
-        self.port = port
+    def __init__(self):
+        super(multiprocessing.Process, self).__init__()
 
-    def startHTTPServer(self):
-        self.webServer = HTTPServer((self.hostName, self.port), RequestHandler)
-        print("Server started http://%s:%s" % (self.hostName, self.port))
-        try:
-            self.webServer.serve_forever()
-        except KeyboardInterrupt:
-            pass
-
-        self.webServer.server_close()
-        print("Server stopped.")
-
-    def startStreamServer(self):
-        address = ('', 8000)
-        streamServer = StreamingServer(address, StreamingHandler)
+    def run(self):
         try:
             # Set up and start the streaming server
-            streamServer.serve_forever()
+            self.serve_forever()
         finally:
             # Stop recording when the script is interrupted
             print("Stream stopped.")
 
 
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(
-            bytes("<html><head><title>Curing Machine</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(
-            bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+class Server():
+    port = 8080
+    cameras = list()
+    webServer: StreamingServer
+
+    def startStreamServer():
+        address = ('', 8000)
+        # self.cameras[0].startStream()
+        streamServer = StreamingServer(address, StreamingHandler)
+        streamServer.start()
+
+    def addCamera(camera: Camera):
+        Server.cameras.append(camera)
 
 
 # Class to handle HTTP requests
@@ -104,14 +94,11 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                 logging.warning(
                     'Removed streaming client %s: %s',
                     self.client_address, str(e))
+        elif self.path == '/button/startcam':
+            Server.cameras[0].startStream()
+        elif self.path == '/button/stopcam':
+            Server.cameras[0].stopStream()
         else:
             # Handle 404 Not Found
             self.send_error(404)
             self.end_headers()
-
-# Class to handle streaming server
-
-
-class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
-    allow_reuse_address = True
-    daemon_threads = True
