0970a18e1484465765c95f97680c87b1eb7cc0ca
enemieltje <enemieltje@gmail.com>
2024-04-26 12:38:58 +0200

    typo typo

--- src/server.py
+++ src/server.py
@@ -59,7 +59,7 @@ class Server():
     def stop():
         Server.streamServer.stop()
         Server.streamServer.server_close()
-        for camera in server.cameras:
+        for camera in Server.cameras:
             camera.stopStream()
 
     def addCamera(camera: Camera):
@@ -108,14 +108,14 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
                     self.client_address, str(e))
         elif self.path == '/button/startcam':
             print("start cam")
-            for camera in server.cameras:
+            for camera in Server.cameras:
                 camera.startStream()
             self.send_response(301)
             self.send_header('Location', '/index.html')
             self.end_headers()
         elif self.path == '/button/stopcam':
             print("stop cam")
-            for camera in server.cameras:
+            for camera in Server.cameras:
                 camera.stopStream()
             self.send_response(301)
             self.send_header('Location', '/index.html')
