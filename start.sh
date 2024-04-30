572ee926d04e0f60cdca7b05322f444be5a069e6
enemieltje <enemieltje@gmail.com>
2024-04-30 16:20:28 +0200

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,18 @@
+# Create all the required files and folders
 mkdir logs
+mkdir pictures
 mv logs/latest.log logs/old.log
 touch logs/latest.log
 
+# Create a virtual environment (venv)
 python -m venv --system-site-packages ./.venv
 
+# Install python packages into the venv
 ./.venv/bin/pip install numpy --upgrade
-./.venv/bin/pip install lgpio pigpio gpio
-./.venv/bin/pip install gpiozero    # Button input
-./.venv/bin/pip install hx711       # Load cell
-./.venv/bin/pip install picamera2   # Camera
+./.venv/bin/pip install lgpio pigpio gpio   # gpio pins
+./.venv/bin/pip install gpiozero            # Button input
+./.venv/bin/pip install hx711               # Load cell
+./.venv/bin/pip install picamera2           # Camera
 
+# Start the program
 ./.venv/bin/python ./src/main.py
