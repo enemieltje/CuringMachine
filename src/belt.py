44011fb8806f85be6e1873238bb75ec37696bfbb
Lord_Redst0ne <74739340+enemieltje@users.noreply.github.com>
2024-05-01 15:30:49 +0200

    reworked belt

--- src/belt.py
+++ src/belt.py
@@ -1,7 +1,6 @@
 import logging
 import multiprocessing
 from config import Config
-import gpiozero as GPIO
 import time
 from DRV8825 import DRV8825
 
@@ -10,20 +9,24 @@ logger = logging.getLogger(__name__)
 
 class Belt:
     # The belt contains two motors with different pins
-    motor1 = DRV8825(dir_pin=13, step_pin=19,
-                     enable_pin=12, mode_pins=(16, 17, 20))
-    motor2 = DRV8825(dir_pin=24, step_pin=18,
-                     enable_pin=4, mode_pins=(21, 22, 27))
+    motors: list
+
+    def configure():
+        Belt.motors[0] = DRV8825(dir_pin=13, step_pin=19,
+                                 enable_pin=12, mode_pins=(16, 17, 20))
+        Belt.motors[1] = DRV8825(dir_pin=24, step_pin=18,
+                                 enable_pin=4, mode_pins=(21, 22, 27))
+        for motor in Belt.motors:
+            motor.SetMicroStep('hardward', '1/4step')
 
     def showcase():
         # Show that the motors are active by turning them a bit
         time.sleep(1)
-        Belt.showcaseMotor(Belt.motor1)
+        Belt.showcaseMotor(Belt.motors[0])
         time.sleep(2)
-        Belt.showcaseMotor(Belt.motor2)
+        Belt.showcaseMotor(Belt.motors[1])
 
     def showcaseMotor(motor: DRV8825):
-        motor.SetMicroStep('hardward', '1/4step')
         motor.TurnStep(Dir='forward', steps=200, stepdelay=0.005)
         time.sleep(0.5)
         motor.TurnStep(Dir='backward', steps=400, stepdelay=0.005)
@@ -31,16 +34,18 @@ class Belt:
 
     def start():
         logger.debug("starting belt")
-        Belt.motor1.SetMicroStep('hardward', '1/4step')
-        Belt.motor2.SetMicroStep('hardward', '1/4step')
         process1 = multiprocessing.Process(
-            target=Belt.startMotor, args=(Belt.motor1))
+            target=Belt.startMotor, args=(0))
         process2 = multiprocessing.Process(
-            target=Belt.startMotor, args=(Belt.motor2))
+            target=Belt.startMotor, args=(1))
         process1.start()
         process2.start()
 
-    def startMotor(motor):
+    def startMotor(index):
         speed = Config.getBeltSpeed()
         logger.debug("starting motor with speed %i", speed)
+        motor = Belt.motors[index]
         motor.TurnStep(Dir='forward', steps=200, stepdelay=1/speed)
+
+
+Belt.configure()
