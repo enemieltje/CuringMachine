03396ae137d92f2c547f9bee0695a3d638e7de9d
Lord_Redst0ne <74739340+enemieltje@users.noreply.github.com>
2024-05-22 11:26:16 +0200

    added i2c pip packages

--- start.sh
+++ start.sh
@@ -10,11 +10,11 @@ python -m venv --system-site-packages ./.venv
 
 # Install python packages into the venv
 ./.venv/bin/pip install numpy --upgrade
-./.venv/bin/pip install lgpio pigpio gpio   # gpio pins
+./.venv/bin/pip install Pin lgpio pigpio gpio   # gpio pins
 ./.venv/bin/pip install gpiozero            # Button input
 ./.venv/bin/pip install hx711               # Load cell
 ./.venv/bin/pip install picamera2           # Camera
-./.venv/bin/pip install python_lcd upymenu # LCD Menu
+./.venv/bin/pip install I2C python_lcd upymenu # LCD Menu
 
 
 # Start the program
